defb4a40c7cf5bfeffe5f6365e74251b929919be
Dinesh Gamage <dinesh@ecyber.com>
2020-05-06 15:25:30 +0530

    updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,26 +4,23 @@ import * as GridLayout from "react-grid-layout";
 declare global {
 
     interface Window {
-        Widgets: any
+        // Widgets: any
     }
 }
-let Widgets = window.Widgets;
-if(typeof Widgets == "undefined") {
-    Widgets = [];
-}
-console.log("______Widgets__________");
-console.log(Widgets);
 
-interface IProps { }
+
+interface IProps {
+    widgets: any[]
+ }
 
 interface IState {
-    showEditDashboard: boolean,
-    enableResize: boolean,
-    enableDrag: boolean,
-    showAddWidget: boolean,
-    newWidget: string,
-    availableWidgets: any[],
-    selectedWidgets: any[],
+    // showEditDashboard: boolean,
+    // enableResize: boolean,
+    // enableDrag: boolean,
+    // showAddWidget: boolean,
+    // newWidget: string,
+    // availableWidgets: any[],
+    // selectedWidgets: any[],
     width: number
 }
 
@@ -33,13 +30,13 @@ class Dashboard extends React.Component<IProps, IState> {
         super(props);
 
         this.state = {
-            showEditDashboard: false,
-            enableDrag: false,
-            enableResize: false,
-            showAddWidget: false,
-            newWidget: "",
-            selectedWidgets: [],
-            availableWidgets: Widgets,
+            // showEditDashboard: false,
+            // enableDrag: false,
+            // enableResize: false,
+            // showAddWidget: false,
+            // newWidget: "",
+            // selectedWidgets: [],
+            // availableWidgets: Widgets,
             width: 1200
         }
 
@@ -58,166 +55,168 @@ class Dashboard extends React.Component<IProps, IState> {
         // let navElement = document.querySelector(".navigation-block") as HTMLElement;
         // console.log("width " + width );
         // console.log("nav width " + navElement.clientWidth);
-        this.setState({ width: width - 200 }, this.loadFromLocalStorage);
+        this.setState({ width: width - 200 });
     }
 
     loadFromLocalStorage() {
-        console.log("__loading_from_LS__")
-        let savedToLS = JSON.parse(localStorage.getItem("selectedWidgets"));
-        if(savedToLS == null) savedToLS = [];
-        console.log(savedToLS);
+        // console.log("__loading_from_LS__")
+        // let savedToLS = JSON.parse(localStorage.getItem("selectedWidgets"));
+        // if(savedToLS == null) savedToLS = [];
+        // console.log(savedToLS);
 
-        let __Widgets = Widgets;
+        // let __Widgets = Widgets;
 
-        let selectedWidgets:any[] = [];
+        // let selectedWidgets:any[] = [];
 
-        savedToLS.map((saved: any, i: number) => {
+        // savedToLS.map((saved: any, i: number) => {
 
-            console.log("__save");
-            console.log(savedToLS);
+        //     console.log("__save");
+        //     console.log(savedToLS);
 
-            // get selected widget
-            let selectedWidget = __Widgets.find((widget: any, j: number) => {
-                if (saved.name == widget.name) {
-                    return widget;
-                }
-            })
+        //     // get selected widget
+        //     let selectedWidget = __Widgets.find((widget: any, j: number) => {
+        //         if (saved.name == widget.name) {
+        //             return widget;
+        //         }
+        //     })
 
             
-            if (typeof selectedWidget !== "undefined") {
-                selectedWidget.layout = saved.layout;
-                selectedWidget.layout.i = i.toString();
-                selectedWidgets.push(selectedWidget);
-            }
-        })
+        //     if (typeof selectedWidget !== "undefined") {
+        //         selectedWidget.layout = saved.layout;
+        //         selectedWidget.layout.i = i.toString();
+        //         selectedWidgets.push(selectedWidget);
+        //     }
+        // })
 
 
-        this.setState({ selectedWidgets: selectedWidgets }, this.getAvailableWidgets);
+        // this.setState({ selectedWidgets: selectedWidgets }, this.getAvailableWidgets);
     }
 
     saveToLocalStorage() {
-        let selectedWidgets = this.state.selectedWidgets;
+        // let selectedWidgets = this.state.selectedWidgets;
 
-        let saveToLS = selectedWidgets.map((widget: any, i: number) => {
-            return { "name": widget.name, "layout": widget.layout };
-        })
+        // let saveToLS = selectedWidgets.map((widget: any, i: number) => {
+        //     return { "name": widget.name, "layout": widget.layout };
+        // })
 
-        if (!this.state.showEditDashboard) {
-            console.log("__saving_to_LS__");
-            console.log(saveToLS);
-            localStorage.setItem("selectedWidgets", JSON.stringify(saveToLS));
-        }
+        // if (!this.state.showEditDashboard) {
+        //     console.log("__saving_to_LS__");
+        //     console.log(saveToLS);
+        //     localStorage.setItem("selectedWidgets", JSON.stringify(saveToLS));
+        // }
     }
 
     getAvailableWidgets() {
-        let selectedWidgets: any[] = [];
+    //     let selectedWidgets: any[] = [];
         
-        this.state.selectedWidgets.map((widget:any, i: number) => {
-            selectedWidgets.push(widget.name)
-        });
+    //     this.state.selectedWidgets.map((widget:any, i: number) => {
+    //         selectedWidgets.push(widget.name)
+    //     });
 
-        let availableWidgets = Widgets.filter((widget:any, j:number) => {
-            if(selectedWidgets.indexOf(widget.name) == -1) {
-                return widget;
-            }
-        })
+    //     let availableWidgets = Widgets.filter((widget:any, j:number) => {
+    //         if(selectedWidgets.indexOf(widget.name) == -1) {
+    //             return widget;
+    //         }
+    //     })
 
 
-        console.log("available widgets")
-        console.log(availableWidgets);
-        this.setState({availableWidgets: availableWidgets});
+    //     console.log("available widgets")
+    //     console.log(availableWidgets);
+    //     this.setState({availableWidgets: availableWidgets});
     }
 
     onLayoutChange(layouts: any) {
-        console.log("___layout___");
-        console.log(layouts);
-        let selectedWidgets = this.state.selectedWidgets;
+        // console.log("___layout___");
+        // console.log(layouts);
+        // let selectedWidgets = this.state.selectedWidgets;
 
-        selectedWidgets.map((widget: any, i: number) => {
-            let layout = layouts[i];
-            widget.layout = layout;
-        })
+        // selectedWidgets.map((widget: any, i: number) => {
+        //     let layout = layouts[i];
+        //     widget.layout = layout;
+        // })
 
-        this.setState({ selectedWidgets: selectedWidgets });
+        // this.setState({ selectedWidgets: selectedWidgets });
     }
 
 
     // add widgets
     toggleAddWidgets() {
-        let show = this.state.showAddWidget;
-        this.setState({ showAddWidget: !show, newWidget: "" });
+        // let show = this.state.showAddWidget;
+        // this.setState({ showAddWidget: !show, newWidget: "" });
     }
 
     onAddNewWidget() {
-        console.log("__add_new_widget__" + this.state.newWidget);
+        // console.log("__add_new_widget__" + this.state.newWidget);
 
-        // get selecrted widget
-        let widget = Widgets.find((widget: any, i: number) => {
-            if (this.state.newWidget == widget.name) {
-                return widget;
-            }
-        });
+        // // get selecrted widget
+        // let widget = Widgets.find((widget: any, i: number) => {
+        //     if (this.state.newWidget == widget.name) {
+        //         return widget;
+        //     }
+        // });
 
-        // update state
-        // enable dashboard edit mode
-        let selectedWidgets = this.state.selectedWidgets;
+        // // update state
+        // // enable dashboard edit mode
+        // let selectedWidgets = this.state.selectedWidgets;
 
-        let layout = { x: 0, y: 0, w: 6, h: 8 };
+        // let layout = { x: 0, y: 0, w: 6, h: 8 };
 
-        if (selectedWidgets.length > 0) {
-            let last = selectedWidgets[selectedWidgets.length - 1];
-            let ll = last.layout;
+        // if (selectedWidgets.length > 0) {
+        //     let last = selectedWidgets[selectedWidgets.length - 1];
+        //     let ll = last.layout;
 
-            layout.y = ll.y + ll.h + 1;
-            if ((ll.x + ll.w) < 12 && (ll.x + ll.w + 6) <= 12) {
-                layout.x = ll.x + ll.w + 1;
-                layout.y = ll.y
-            }
-            console.log(layout);
-        }
+        //     layout.y = ll.y + ll.h + 1;
+        //     if ((ll.x + ll.w) < 12 && (ll.x + ll.w + 6) <= 12) {
+        //         layout.x = ll.x + ll.w + 1;
+        //         layout.y = ll.y
+        //     }
+        //     console.log(layout);
+        // }
 
-        widget.layout = layout;
-        selectedWidgets.push(widget);
+        // widget.layout = layout;
+        // selectedWidgets.push(widget);
 
-        this.setState({
-            selectedWidgets: selectedWidgets
-        }, () => { 
-            this.toggleAddWidgets(); 
-            if(!this.state.showEditDashboard) this.toggleDashboardEdit(); 
-            this.getAvailableWidgets();
-        });
+        // this.setState({
+        //     selectedWidgets: selectedWidgets
+        // }, () => { 
+        //     this.toggleAddWidgets(); 
+        //     if(!this.state.showEditDashboard) this.toggleDashboardEdit(); 
+        //     this.getAvailableWidgets();
+        // });
 
     }
 
     // edit widgets
     toggleDashboardEdit() {
-        let show = this.state.showEditDashboard;
-        this.setState({
-            showEditDashboard: !show,
-            enableDrag: !show,
-            enableResize: !show
-        }, this.saveToLocalStorage);
+        // let show = this.state.showEditDashboard;
+        // this.setState({
+        //     showEditDashboard: !show,
+        //     enableDrag: !show,
+        //     enableResize: !show
+        // }, this.saveToLocalStorage);
     }
     
     removeWidget(key:number){
-        let selectedWidgets = this.state.selectedWidgets;
-        selectedWidgets.splice(key, 1);
+        // let selectedWidgets = this.state.selectedWidgets;
+        // selectedWidgets.splice(key, 1);
 
-        this.setState({selectedWidgets: selectedWidgets}, this.getAvailableWidgets);
+        // this.setState({selectedWidgets: selectedWidgets}, this.getAvailableWidgets);
     } 
 
     // render
     renderWidget(widget: any, key: number) {
         let WidgetElement = widget.widget;
+        let layout = { x: 0, y: 0, w: 6, h: 8 };
+
         return (
-            <div className="layout-item" key={key} data-grid={widget.layout}>
-                {
+            <div className="layout-item" key={key} data-grid={layout}>
+                {/* {
                     this.state.showEditDashboard ? 
                     <div className="overlay">
                         <button className="remove-widget" onClick={() => this.removeWidget(key)} >Remove</button>
                     </div>
                     :""
-                }
+                } */}
 
                 <WidgetElement />
             </div>
@@ -255,7 +254,7 @@ class Dashboard extends React.Component<IProps, IState> {
                     <div className="content-block">
 
                         <div className="toolbar">
-                            <div className="toolbar-buttons">
+                            {/* <div className="toolbar-buttons">
                                 <button className={`toggleEdit ${this.state.showEditDashboard ? "active" : ""}`} onClick={this.toggleDashboardEdit}> {this.state.showEditDashboard ? "Save Changes" : "Edit Dashboard"} </button>
                             </div>
                             <div className="toolbar-buttons">
@@ -281,21 +280,21 @@ class Dashboard extends React.Component<IProps, IState> {
                                     }
                                 </div>
 
-                            </div>
+                            </div> */}
                         </div>
 
                         <GridLayout className="layout"
                             cols={12}
                             rowHeight={30}
                             width={this.state.width}
-                            isResizable={this.state.enableResize}
-                            isDraggable={this.state.enableDrag}
+                            isResizable={true}
+                            isDraggable={true}
                             autoSize={false}
                             onLayoutChange={this.onLayoutChange}
                         >
 
                             {
-                                this.state.selectedWidgets.map((widget: any, key: number) => this.renderWidget(widget, key))
+                                this.props.widgets.map((widget: any, key: number) => this.renderWidget(widget, key))
                             }
 
                         </GridLayout>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import * as ReactDOM from "react-dom";
 import "./index.scss";
 import Dashboard from "./components/Dashboard";
 
-ReactDOM.render(
-    <Dashboard />,
-    document.getElementById("root")
-);
+// ReactDOM.render(
+//     <Dashboard />,
+//     document.getElementById("root")
+// );
+
+
+(window as any).renderDashboard = (root: string, widgets: any[]) => {
+    ReactDOM.render(<Dashboard widgets={widgets} />,document.getElementById('root'));
+}
